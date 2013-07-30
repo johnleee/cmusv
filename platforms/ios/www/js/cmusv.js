523a24287a0659de3e165f0d83ee6a4e7e95ae46
John Lee <john.lee@sv.cmu.edu>
2013-07-30 00:45:28 -0700

    add Alumni section and use InAppBrowser for links

--- platforms/ios/www/js/cmusv.js
+++ platforms/ios/www/js/cmusv.js
@@ -8,13 +8,35 @@
           var container = document.getElementById("feed");
           for (var i = 0; i < result.feed.entries.length; i++) {
             var entry = result.feed.entries[i];
-            console.log(entry);
-            var link = $("<a>", {"href": entry.link, "target": "_blank"});
+            //console.log(entry);
+            var link = $("<a>", {"href": "#", "target": "_blank", "onclick": "openWindow('" + entry.link +"');return false;"});
             var summary = $("<p style='padding-left:15px;padding-right:35px;'>", {"size": "2"});
             $("#feed").append($("<li>").append(link.html(entry.title)).append(summary.html(entry.content)));
           }
            $("#feed").listview('refresh');
         }
       });
+
+     var alumFeed = new google.feeds.Feed("http://www.cmu.edu/silicon-valley/news-events/news/alumni-rss-feed.rss");
+      alumFeed.setNumEntries(10);
+      alumFeed.load(function(result) {
+        if (!result.error) {
+          var container = document.getElementById("alumFeed");
+          for (var i = 0; i < result.feed.entries.length; i++) {
+            var entry = result.feed.entries[i];
+            console.log(entry);
+            //var link = $("<a>", {"href": entry.link, "target": "_blank"});
+            var link = $("<a>", {"href": "#", "target": "_blank", "onclick": "openWindow('" + entry.link +"');return false;"});
+            var summary = $("<p style='padding-left:15px;padding-right:35px;'>", {"size": "2"});
+            $("#alumFeed").append($("<li>").append(link.html(entry.title)).append(summary.html(entry.content)));
+          }
+           $("#alumFeed").listview('refresh');
+        }
+      });
+
     }
     google.setOnLoadCallback(initialize);
+
+    function openWindow(url) {
+        var ref = window.open(url, '_blank', 'location=yes,enableViewportScale=yes');
+    }
